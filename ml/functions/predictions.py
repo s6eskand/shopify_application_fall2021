c9998be01de9e5c6b2264dac4fb77e8dacd953a7
Sam <sam.eskandar00@hotmail.com>
2021-04-28 17:06:44 -0400

    add prediction logic

---
+++ ml/functions/predictions.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import pickle
+
+
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+def load_image(image):
+    # img = tf.io.read_file(image_path)
+    img = tf.image.decode_image(image, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img
+
+
+def generate_caption(encoder, decoder, image):
+    image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+    new_input = image_model.input
+    hidden_layer = image_model.layers[-1].output
+
+    image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+    with open('../app/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    max_length = 50
+
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dims(load_image(image), 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result
