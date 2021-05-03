6b17fceadf31e8684997aa5d40058778f2698460
Sam <sam.eskandar00@hotmail.com>
2021-05-03 03:00:58 -0400

    create loading indicator on navigation

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
+import NProgress from 'nprogress';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    const router = useRouter();
+
+    useEffect(() => {
+        let routeChangeStart = () => NProgress.start();
+        let routeChangeComplete = () => NProgress.done();
+
+        router.events.on("routeChangeStart", routeChangeStart);
+        router.events.on("routeChangeComplete", routeChangeComplete);
+        router.events.on("routeChangeError", routeChangeComplete);
+        return () => {
+            router.events.off("routeChangeStart", routeChangeStart);
+            router.events.off("routeChangeComplete", routeChangeComplete);
+            router.events.off("routeChangeError", routeChangeComplete);
+        };
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
