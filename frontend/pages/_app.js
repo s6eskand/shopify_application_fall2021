7e72397f5860aa56d20daec14a1e2486a9f9769f
Sam <sam.eskandar00@hotmail.com>
2021-05-07 16:57:29 -0400

    logout on page close

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,12 +3,27 @@ import NProgress from 'nprogress';
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { logoutRequest } from "../src/api/auth";
 
 function MyApp({ Component, pageProps }) {
 
     const router = useRouter();
 
+    const logout = (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem("AUTH_TOKEN");
+        if (token) {
+            logoutRequest(token)
+                .then(res => res);
+        }
+        localStorage.clear();
+        return undefined;
+    }
+
     useEffect(() => {
+
+        window.addEventListener("beforeunload", logout);
+
         let routeChangeStart = () => NProgress.start();
         let routeChangeComplete = () => NProgress.done();
 
@@ -19,6 +34,7 @@ function MyApp({ Component, pageProps }) {
             router.events.off("routeChangeStart", routeChangeStart);
             router.events.off("routeChangeComplete", routeChangeComplete);
             router.events.off("routeChangeError", routeChangeComplete);
+            window.removeEventListener("beforeunload", logout)
         };
     }, []);
 
